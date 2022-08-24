35f85a2fc065e32ddb84a9e0857ae82c3043070a
EzequielCastilloH <emcastillo2@espe.edu.ec>
2022-08-24 14:58:44 -0500

    Traduccion

--- chatapp.py
+++ chatapp.py
@@ -27,19 +27,19 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# devuelve la matriz de palabras de la bolsa: 0 o 1 para cada palabra de la bolsa que existe en la frase
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+    # tokenizar el patrón
     sentence_words = clean_up_sentence(sentence)
     
-    # bag of words - matrix of N words, vocabulary matrix
+    # bolsa de palabras - matriz de N palabras, matriz de vocabulario
     bag = [0]*len(words)
     
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s:
                 
-                # assign 1 if current word is in the vocabulary position
+                # asigna 1 si la palabra actual está en la posición del vocabulario
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
@@ -47,13 +47,13 @@ def bow(sentence, words, show_details=True):
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
+    # filtrar las predicciones por debajo de un umbral
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     
-    # sort by strength of probability
+    # ordenar por fuerza de la probabilidad
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     
@@ -78,7 +78,7 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-#Creating GUI with tkinter
+#Creación de GUI con tkinter
 def send():
     msg = EntryBox.get("1.0",'end-1c').strip()
     EntryBox.delete("0.0",END)
@@ -100,26 +100,26 @@ base.title("Bienvenido al ChatBot")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
+#Crear una ventana de chat
 ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
 ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
+#Unir la barra de desplazamiento a la ventana del chat
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
+#Crear un botón para enviar un mensaje
 SendButton = Button(base, font=("verdana",12,'bold'), text="Enviar \n mensaje", width="8", height=5,
                     bd=0, bg="#798e93", activebackground="#b4bfbf",fg='#ffffff',
                     command= send )
 
-#Create the box to enter message
+#Crear la caja para introducir el mensaje
 EntryBox = Text(base, bd=0, bg="#dbd5c5",width="29", height="5", font="Arial")
 #EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
+#Coloca todos los componentes en la pantalla
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
 EntryBox.place(x=128, y=401, height=90, width=265)
--- chatbot.py
+++ chatbot.py
@@ -28,14 +28,14 @@ words = []
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        # take each word and tokenize it
+        # tomar cada palabra y tokenizarla
         w = nltk.word_tokenize(pattern)
         words.extend(w)
         
-        # adding documents
+        # añadir documentos
         documents.append((w, intent['tag']))
 
-        # adding classes to our class list
+        # añadiendo clases a nuestra lista de clases
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
@@ -54,43 +54,42 @@ print (len(words), "unique lemmatized words", words)
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
-# initializing training data
+# inicialización de los datos de entrenamiento
 training = []
 output_empty = [0] * len(classes)
 
 for doc in documents:
     
-    # initializing bag of words
+    # inicialización de la bolsa de palabras
     bag = []
     
-    # list of tokenized words for the pattern
+    # lista de palabras tokenizadas para el patrón
     pattern_words = doc[0]
     
-    # lemmatize each word - create base word, in attempt to represent related words
+    # lematizar cada palabra - crear la palabra base, en un intento de representar palabras relacionadas
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
     
-    # create our bag of words array with 1, if word match found in current pattern
+    # crear nuestra matriz de bolsa de palabras con 1, si la palabra coincide con el patrón actual
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    # la salida es un '0' para cada etiqueta y un '1' para la etiqueta actual (para cada patrón)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
 
     training.append([bag, output_row])
     
-# shuffle our features and turn into np.array
+# barajar nuestras características y convertirlas en np.array
 random.shuffle(training)
 training = np.array(training)
 
-# create train and test lists. X - patterns, Y - intents
+# crear listas de entrenamiento y de prueba. X - patrones, Y - intentos
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# Crear modelo - 3 capas. Primera capa 128 neuronas, segunda capa 64 neuronas y tercera capa de salida contiene un número de neuronas
+# igual al número de intenciones para predecir la intención de salida con softmax
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -98,11 +97,11 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compilar el modelo. El descenso de gradiente estocástico con gradiente acelerado de Nesterov da buenos resultados para este modelo
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model
+#ajustar y guardar el modelo
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
